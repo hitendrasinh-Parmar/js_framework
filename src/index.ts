b05c1bba823516989f0fe8a9f224c0d4828dad30
hitendrasinh-Parmar <hitrajput098@gmail.com>
2023-02-01 07:24:41 +0530

    sync wuth user

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { User } from "./models/user";
 
 const user = new User({ id: 1 });
 
-user.set({ 'age': 200, name: 'NWWW' });
 // user.save();
 
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,43 +1,32 @@
-import axios, { AxiosResponse } from "axios";
-import { Eventing } from "./Eventing";
 
-interface UserProps {
+import { Eventing } from "./Eventing";
+import { Sync } from './Sync';
+import { Attributes } from "./Attributes";
+
+export interface UserProps {
   id?: number,
   name?: string,
   age?: number
 }
 
+const rootUrl = 'https://localhost:3000/users';
+
 export class User {
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  public event: Eventing = new Eventing();
-
-  constructor(
-    private data: UserProps,
-  ) { }
-
-  get(propName: string): number | string {
-    return this.data[propName];
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs)
   }
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
+  get on() {
+    return this.events.on;
   }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      })
+  get trigger() {
+    return this.events.trigger;
   }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
+  get get() {
+    return this.attributes.get;
   }
-}
-
+} 
